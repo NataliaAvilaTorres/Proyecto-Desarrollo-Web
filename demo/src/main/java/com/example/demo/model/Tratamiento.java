234c956ea71bee03c3e89365c161ec599555ee77
inatalia.avila <inatalia.avila@javeriana.edu.co>
2024-09-14 17:52:37 -0500

    BD actualizada

---
+++ demo/src/main/java/com/example/demo/model/Tratamiento.java
@@ -0,0 +1,80 @@
+package com.example.demo.model;
+
+import java.sql.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Tratamiento {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private Date fecha;
+
+    @ManyToOne
+    @JoinColumn(name = "id_mascota", nullable = false)
+    private Mascota mascota;
+
+    @ManyToOne
+    @JoinColumn(name = "cedula_veterinario", nullable = false)
+    private Veterinario veterinario;
+
+    @ManyToOne
+    @JoinColumn(name = "nombre_medicamento", nullable = false)
+    private Medicamento medicamento;
+
+
+    public Tratamiento() {
+    }
+
+    public Tratamiento(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Mascota getMascota() {
+        return mascota;
+    }
+
+    public void setMascota(Mascota mascota) {
+        this.mascota = mascota;
+    }
+
+    public Veterinario getVeterinario() {
+        return veterinario;
+    }
+
+    public void setVeterinario(Veterinario veterinario) {
+        this.veterinario = veterinario;
+    }
+
+    public Medicamento getMedicamento() {
+        return medicamento;
+    }
+
+    public void setMedicamento(Medicamento medicamento) {
+        this.medicamento = medicamento;
+    }
+
+}
